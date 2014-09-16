e4f978dcf33048a186e5d1c57342636b84d5e9b8
Adam Parrish <aparrish@neosavvy.com>
2014-09-16 09:23:30 -0400

    The Altimeter spec from the book appears to work immediately.

---
+++ src/test/scala/com/neosavvy/avionics/AltimeterSpec.scala
@@ -0,0 +1,76 @@
+package com.neosavvy.avionics
+
+import akka.actor.{Props, Actor, ActorSystem}
+import akka.testkit.{TestActorRef, TestLatch, ImplicitSender, TestKit}
+
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+/**
+ * Created by aparrish on 9/16/14.
+ */
+class AltimeterSpec extends TestKit(ActorSystem("AltimeterSpec"))
+                    with ImplicitSender
+                    with WordSpecLike
+                    with Matchers
+                    with BeforeAndAfterAll {
+
+  import Altimeter._
+  override def afterAll() { system.shutdown() }
+
+  class Helper {
+
+    object EventSourceSpy {
+      val latch = TestLatch(1)
+    }
+
+    trait EventSourceSpy extends EventSource {
+      def sendEvent[T](event : T) : Unit = EventSourceSpy.latch.countDown()
+
+      def eventSourceReceive = Actor.emptyBehavior
+    }
+
+    def slicedAltimeter = new Altimeter with EventSourceSpy
+
+    def actor() = {
+      val a = TestActorRef[Altimeter](Props(slicedAltimeter))
+      (a, a.underlyingActor)
+    }
+
+  }
+
+  "Altimeter" should {
+    "record rate of climb changes" in new Helper {
+      val (_, real) = actor()
+      real.receive(RateChange(1f))
+      real.rateOfClimb should be (real.maxRateOfClimb)
+    }
+    "keep rate of climb changes within bounds" in new Helper {
+      val (_, real) = actor()
+      real.receive(RateChange(2f))
+      real.rateOfClimb should be (real.maxRateOfClimb)
+    }
+    "calculate altitude changes" in new Helper {
+      val ref = system.actorOf(Props(Altimeter()))
+      ref ! EventSource.RegisterListener(testActor)
+      ref ! RateChange(1f)
+      fishForMessage() {
+        case AltitudeUpdate(altitude) if altitude == 0f => false
+        case AltitudeUpdate(altitude) => true
+      }
+    }
+
+    "send events" in new Helper {
+      val (ref, _) = actor()
+      Await.ready(EventSourceSpy.latch, 1.second)
+      EventSourceSpy.latch.isOpen should be (true)
+    }
+  }
+
+
+
+
+
+}
