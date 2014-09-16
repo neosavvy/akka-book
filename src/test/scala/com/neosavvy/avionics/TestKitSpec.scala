5061c88305ba01178b8787bb0847546cc98b8783
Adam Parrish <aparrish@neosavvy.com>
2014-09-16 09:10:23 -0400

    Cleaning up the TeskKitSpec before I move on to testing the altimeter.

--- src/test/scala/com/neosavvy/avionics/TestKitSpec.scala
+++ src/test/scala/com/neosavvy/avionics/TestKitSpec.scala
@@ -6,36 +6,12 @@ import akka.actor.{Actor, Props, ActorRef, ActorSystem}
 import akka.testkit.{ImplicitSender, TestKit}
 import com.neosavvy.avionics.MyActor.{Ping, Pong}
 import org.scalatest._
-import org.scalatest.fixture.{ NoArg, UnitFixture }
+import org.scalatest.fixture.{UnitFixture}
 
 
 object ActorSys {
   val uniqueId = new AtomicInteger(0)
 }
-//class ActorSys(name: String) extends
-//TestKit(ActorSystem(name))
-//  with ImplicitSender
-//with DelayedInit
-//with (() => Unit) {
-//
-//  private var theBody: () => Unit = _
-//
-//  def this() = this(
-//    "TestSystem%05d".format(
-//    ActorSys.uniqueId.getAndIncrement()))
-//
-//  def shutdown():Unit = system.shutdown()
-//
-//  final def delayedInit(body: => Unit) {
-//    theBody = (() => body)
-//  }
-//
-//  override def apply(): Unit = {
-//    try theBody()
-//    finally shutdown()
-//  }
-//
-//}
 
 class ActorSys(name: String) extends
 TestKit(ActorSystem(name))
@@ -46,7 +22,7 @@ with fixture.NoArg {
     "TestSystem%05d".format(
       ActorSys.uniqueId.getAndIncrement()))
 
-  def shutdown():Unit = system.shutdown()
+  def shutdown(): Unit = system.shutdown()
 
   override def apply(): Unit = {
     try super.apply()
@@ -55,18 +31,18 @@ with fixture.NoArg {
 
 }
 
-//class TestKitSpec( actorSystem : ActorSystem) extends TestKit(actorSystem)
-//  with WordSpecLike with Matchers with BeforeAndAfterAll
-
 object MyActor {
+
   case class Ping()
+
   case class Pong()
+
 }
 
 
 class MyActor extends Actor {
 
-  def receive : Receive = {
+  def receive: Receive = {
     case Ping => {
       println("Receiving a ping....")
       sender ! Pong
@@ -81,21 +57,27 @@ with Matchers
 with ParallelTestExecution
 with UnitFixture {
 
-  class SubActorSys(name : String) extends ActorSys(name) {
-    def makeActor(): ActorRef = system.actorOf(Props(new MyActor), "MyActor")
+  class SubActorSys(name: String) extends ActorSys(name) {
+    def makeActor(): ActorRef = system.actorOf(Props(new MyActor()))
   }
-//  def makeActor() : ActorRef = system.actorOf(Props[MyActor], "MyActor")
 
   "My Actor" should {
-    "throw if constructed with the wrong name" ignore new ActorSys {
+
+    /**
+     * The actor will throw an exception if the name contains a blank
+     * space at the beginning of the name param
+     *
+     * see http://doc.akka.io/docs/akka/snapshot/scala/actors.html
+     */
+    "throw if constructed with the wrong name" in new ActorSys {
       evaluating {
-        val a = system.actorOf(Props[MyActor])
-      } should produce [Exception]
+        val a = system.actorOf(Props[MyActor], name=" ")
+      } should produce[Exception]
     }
-    "construct without exception" in new SubActorSys("Test1")  {
+    "construct without exception" in new SubActorSys("Test1") {
       val a = makeActor()
     }
-    "respond with a Pong to a Ping" in new SubActorSys("Test2")  {
+    "respond with a Pong to a Ping" in new SubActorSys("Test2") {
       val a = makeActor()
       a ! Ping
       expectMsg(Pong)
